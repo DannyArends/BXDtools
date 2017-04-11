fb9270efad849abd7b45a8ff5f48e6f07781d81f
DannyArends <Danny.Arends@Gmail.com>
2017-04-11 11:15:16 +0200

    Two functions to deal with allele frequencies in the BXD

---
+++ R/BXD-allelefrequecies.R
@@ -0,0 +1,34 @@
+#
+# BXD-allelefrequencies.R
+#
+# copyright (c) 2017-2020 - Danny Arends, Pjotr Prins, Rob Williams, Gudrun A. Brockmann
+# last modified Apr, 2017
+# first written Apr, 2017
+# 
+# Routines to calculate allele frequencies
+#
+
+calc.alleleFrequencies <- function(bxd.genotypes) {
+  allelefreq <- apply(bxd.genotypes, 1, function(x){
+    tbl <- table(x)
+    return(c((tbl["B"] * 2 + tbl["H"]) / (2* sum(tbl)), (tbl["D"] * 2 + tbl["H"]) / (2* sum(tbl))))
+  })
+  allelefreq <- t(allelefreq)
+  allelefreq[,1] <- round(as.numeric(allelefreq[,1]),d = 3)
+  allelefreq[,2] <- round(as.numeric(allelefreq[,2]),d = 3)
+  colnames(allelefreq) <- c("B", "D")
+  return(allelefreq)
+}
+
+calc.MAF <- function(bxd.genotypes) {
+  allelefreq <- apply(bxd.genotypes, 1, function(x){
+    tbl <- table(x)
+    if(tbl["B"] < tbl["D"])  return( c("B", (tbl["B"] * 2 + tbl["H"]) / (2* sum(tbl))) )
+    if(tbl["D"] < tbl["B"])  return( c("D", (tbl["D"] * 2 + tbl["H"]) / (2* sum(tbl))) )
+    if(tbl["D"] == tbl["B"]) return( c("E", 0.5) )
+  })
+  allelefreq <- t(allelefreq)
+  allelefreq[,2] <- round(as.numeric(allelefreq[,2]),d = 3)
+  colnames(allelefreq) <- c("Allele", "MAF")
+  return(allelefreq)
+}
