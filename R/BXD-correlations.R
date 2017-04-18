9a44362dc306ded7bd77d4c5fe664e40ada73b8a
DannyArends <Danny.Arends@Gmail.com>
2017-04-18 12:08:22 +0200

    Phenotype to phenotype correlations, now with minimum % of matched phenotype (default 90%)

--- R/BXD-correlations.R
+++ R/BXD-correlations.R
@@ -8,18 +8,41 @@
 # Routines to calculate correlations between phenotypes in BXD mice
 #
 
-calc.correlations <- function(phenotype, bxd.phenotypes, top = 100, method = c("pearson", "kendall", "spearman")) {
+calc.correlations <- function(phenotype, bxd.phenotypes, top = 100, matched = 0.9, method = c("pearson", "kendall", "spearman")) {
+  bxd.phenotypes <- phenotype.add.class(bxd.phenotypes)
+  phenotype.descriptions <- attr(bxd.phenotypes, "annotation")
   bxd.subset <- bxd.phenotypes[,names(phenotype)]
+  perc.missing <- apply(bxd.subset,1,function(x){
+    return(sum(is.na(x))/length(x))
+  })
+  bxd.subset <- bxd.subset[-which(perc.missing >= matched),]
   correlations <- cor(phenotype, t(bxd.subset), use = "pairwise.complete.obs", method = method[1])
   names(correlations) <- rownames(bxd.subset)
   sorted <- sort(abs(correlations), decreasing = TRUE)[1:top]
-  cbind(names(sorted), correlations[names(sorted)])
+  return(data.frame(cbind( phenotype.descriptions[names(sorted),], "R" = round(correlations[names(sorted)], 3))))
 }
 
-test.correlation <- function(){
-  availableBXD <- colnames(bxd.phenotypes)[3:100]
-  phenotype <- rnorm(30)
-  names(phenotype) <- sample(availableBXD, 30)
 
+
+test.correlation <- function(){
+  require("BXDtools")
+
+  data('bxd.phenotypes', package='BXDtools', envir=environment())
+  bxd.phenotypes <- get("bxd.phenotypes", envir=environment()) 
+
+  bxd.phenotypes <- phenotype.add.class(bxd.phenotypes)
+
+  perc.missing <- apply(bxd.phenotypes,2,function(x){
+    return(sum(is.na(x))/length(x))
+  })
+
+  N <- 30
+  availableBXD <- names(which(perc.missing < 0.9))
+  phenotype <- rnorm(N)
+  names(phenotype) <- sample(availableBXD, N)
+
+  calc.correlations(phenotype, bxd.phenotypes, 5)
+  
+  table(attr(bxd.phenotypes, "annotation")[,"class"])
 }
 
